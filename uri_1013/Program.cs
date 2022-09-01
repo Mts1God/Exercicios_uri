d6cbec947eef82f316f01114f665c4544cadb2f9
mateus.fribeiro1 <mateus.fribeiro1@senacsp.edu.br>
2022-09-01 17:20:20 -0300

    atualização

---
+++ uri_1013/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri_1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split();
+
+            int a = int.Parse(valores[0], CultureInfo.InvariantCulture);
+            int b = int.Parse(valores[1], CultureInfo.InvariantCulture);
+            int c = int.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            //Fórmula
+            double MaiorAB = (a + b + Math.Abs(a - b)) / 2;
+            double MaiorABC = (MaiorAB + c + Math.Abs(MaiorAB - c)) / 2;
+
+            Console.WriteLine(+MaiorABC + " eh o maior");
+
+        }
+    }
+}
---
+++ uri_1014/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri_1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int D = int.Parse(Console.ReadLine());
+            double L = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double cm = D / L;
+            Console.WriteLine(cm.ToString("F3", CultureInfo.InvariantCulture) + "km/l");
+
+        }
+    }
+}
