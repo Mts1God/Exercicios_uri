c34a6a56d71a1418ea87f1f47787a9d048d1b90a
mateus.fribeiro1 <mateus.fribeiro1@senacsp.edu.br>
2022-09-12 16:03:52 -0300

    atu

--- uri_1037/Program.cs
+++ uri_1037/Program.cs
@@ -9,19 +9,19 @@ namespace uri_1037
         {
             double num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            if (num >= 0 && num <= 25.000000)
+            if (num >= 0 && num <= 25)
             {
                 Console.WriteLine("Intervalo [0,25]");
             }
-            else if (num >= 25.00001 && num <= 50.000000)
+            else if (num >= 25 && num <= 50)
             {
                 Console.WriteLine("Intervalo (25,50]");
             }
-            else if (num >= 50.00001 && num <= 75.000000)
+            else if (num >= 50 && num <= 75)
             {
                 Console.WriteLine("Intervalo (50,75]");
             }
-            else if (num >= 75.00001 && num <= 100.000000)
+            else if (num >= 75 && num <= 100)
             {
                 Console.WriteLine("Intervalo (75,100]");
             }
