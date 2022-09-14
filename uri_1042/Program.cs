55acee6e06aa06a684600aadf3aba5369dcdd419
mateus.fribeiro1 <mateus.fribeiro1@senacsp.edu.br>
2022-09-14 15:21:49 -0300

    ultima atu

--- uri_1042/Program.cs
+++ uri_1042/Program.cs
@@ -6,29 +6,79 @@ namespace uri_1042
     {
         static void Main(string[] args)
         {
-            string[] valores = Console.ReadLine().Split();
-            int meio = 0;
-            int maior = Math.Max(Math.Max(int.Parse(valores[0]), int.Parse(valores[1])), int.Parse(valores[2]));
-            int menor = Math.Min(Math.Min(int.Parse(valores[0]), int.Parse(valores[1])), int.Parse(valores[2]));
+            //string[] valores = Console.ReadLine().Split();
+            //int meio = 0;
+            //int maior = Math.Max(Math.Max(int.Parse(valores[0]), int.Parse(valores[1])), int.Parse(valores[2]));
+            //int menor = Math.Min(Math.Min(int.Parse(valores[0]), int.Parse(valores[1])), int.Parse(valores[2]));
+            //
+            //for (int x = 0; x < 3; x++)
+            //{
+            //    if (int.Parse(valores[x]) != maior && int.Parse(valores[x]) != menor)
+            //    {
+            //        meio = int.Parse(valores[x]);
+            //        break;
+            //    }
+            //    else if (int.Parse(valores[x]) >= meio)
+            //    {
+            //        meio = int.Parse(valores[x]);
+            //    }
+            //}
+            //Console.WriteLine($"{menor}\n{meio}\n{maior}\n");
+            //
+            //foreach (string valor in valores)
+            //{
+            //    Console.WriteLine(valor);
+            //}
 
-            for (int x = 0; x < 3; x++)
+            string[] numeros = Console.ReadLine().Split(' ');
+            int numOne = int.Parse(numeros[0]);
+            int numTwo = int.Parse(numeros[1]);
+            int numThree = int.Parse(numeros[2]);
+            int menor = 0, medio = 0, maior = 0;
+            if (numOne < numTwo && numOne < numThree && numTwo < numThree)
             {
-                if (int.Parse(valores[x]) != maior && int.Parse(valores[x]) != menor)
-                {
-                    meio = int.Parse(valores[x]);
-                    break;
-                }
-                else if (int.Parse(valores[x]) >= meio)
-                {
-                    meio = int.Parse(valores[x]);
-                }
+                menor = numOne;
+                medio = numTwo;
+                maior = numThree;
             }
-            Console.WriteLine($"{menor}\n{meio}\n{maior}\n");
+            else if (numOne < numTwo && numOne < numThree && numTwo > numThree)
+            {
+                menor = numOne;
+                medio = numThree;
+                maior = numTwo;
+            }
+            else if (numOne > numTwo && numOne < numThree && numTwo < numThree)
+            {
+                menor = numTwo;
+                medio = numOne;
+                maior = numThree;
+            }
+            else if (numOne > numTwo && numOne > numThree && numTwo < numThree)
+            {
+                menor = numTwo;
+                medio = numThree;
+                maior = numOne;
+            }
+            else if (numOne > numTwo && numOne > numThree && numTwo > numThree)
+            {
+                menor = numThree;
+                medio = numTwo;
+                maior = numOne;
+            }
+            else
+            {
+                menor = numThree;
+                medio = numOne;
+                maior = numTwo;
+            }
 
-            foreach (string valor in valores)
-            {
-                Console.WriteLine(valor);
-            }
+            Console.WriteLine(menor);
+            Console.WriteLine(medio);
+            Console.WriteLine(maior);
+            Console.WriteLine("");
+            Console.WriteLine(numOne);
+            Console.WriteLine(numTwo);
+            Console.WriteLine(numThree);
         }
     }
 }
--- uri_1043/Program.cs
+++ uri_1043/Program.cs
@@ -7,21 +7,21 @@ namespace uri_1043
     {
         static void Main(string[] args)
         {
-            string[] valores = Console.ReadLine().Split();
+            string[] valores = Console.ReadLine().Split(' ');
 
             double a = double.Parse(valores[0], CultureInfo.InvariantCulture);
             double b = double.Parse(valores[1], CultureInfo.InvariantCulture);
             double c = double.Parse(valores[2], CultureInfo.InvariantCulture);
 
-            if (a + b > c && b + c > a && c + a > b)
+            if (a < b + c && a > Math.Abs(b - c))
             {
                 double perimetro = a + b + c;
-                Console.WriteLine($"Perimetro = {perimetro.ToString("F1", CultureInfo.InvariantCulture)}");
+                Console.WriteLine("Perimetro = " + perimetro.ToString("F1", CultureInfo.InvariantCulture));
             }
             else
             {
-                double area = ((a + b) / 2) * c;
-                Console.WriteLine($"Area = {area.ToString("F1", CultureInfo.InvariantCulture)}");
+                double area = (a + b) / 2.0 * c;
+                Console.WriteLine("Area = " + area.ToString("F1", CultureInfo.InvariantCulture));
             }
         }
     }
